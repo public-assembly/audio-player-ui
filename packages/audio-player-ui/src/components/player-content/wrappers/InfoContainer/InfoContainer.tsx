ab10f77db5684a166e444ed54e66b585db3c5cfb
Dain Blodorn Kim <dain@db13.us>
2022-09-27 15:21:09 -0700

    add selectors to info container

--- packages/audio-player-ui/src/components/player-content/wrappers/InfoContainer/InfoContainer.tsx
+++ packages/audio-player-ui/src/components/player-content/wrappers/InfoContainer/InfoContainer.tsx
@@ -4,11 +4,11 @@ export const AudioPlayerDisplayInfo = ({}) => {
   const { currentTrack } = usePlayerContext();
   const { artist, title } = currentTrack;
   return (
-    <div className="col-span-2 ">
-      <div className="flex items-center">
-        <span className="">{artist}</span>
-        <span className="mx-1">-</span>
-        <span className="truncate">{title}</span>
+    <div className="pa-audio-player__info-container col-span-2 ">
+      <div className="pa-audio-player__info-container-items flex items-center">
+        <span className="pa-audio-player__info-container-artist">{artist}</span>
+        <span className="pa-audio-player__info-container-hyphen mx-1">-</span>
+        <span className="pa-audio-player__info-container-title truncate">{title}</span>
       </div>
     </div>
   );
