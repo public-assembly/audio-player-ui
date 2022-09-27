200760a5651d94bd6dcd91909956bfada82808bc
Dain Blodorn Kim <dain@db13.us>
2022-09-27 15:23:24 -0700

    add selectors to player wrapper

--- packages/audio-player-ui/src/components/player-content/wrappers/PlayerWrapper/PlayerWrapper.tsx
+++ packages/audio-player-ui/src/components/player-content/wrappers/PlayerWrapper/PlayerWrapper.tsx
@@ -4,8 +4,8 @@ export function PlayerWrapper({
   children?: JSX.Element | JSX.Element[];
 }) {
   return (
-    <div className="flex items-center z-40 fixed bottom-0  left-0 right-0 justify-between  ">
-      <div className="flex flex-col gap-2 xl:grid grid-cols-12 w-[100%] bg-[#ffffff] text-sm text-stone-800 border-t border-t-stone-300 border-l-0 border-b-0 border-r-0  p-6 ">
+    <div className="pa-audio-player__player flex items-center z-40 fixed bottom-0  left-0 right-0 justify-between  ">
+      <div className="pa-audio-player__player-wrapper flex flex-col gap-2 xl:grid grid-cols-12 w-[100%] bg-[#ffffff] text-sm text-stone-800 border-t border-t-stone-300 border-l-0 border-b-0 border-r-0  p-6 ">
         {children}
       </div>
     </div>
