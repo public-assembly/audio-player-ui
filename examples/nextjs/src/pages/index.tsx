aafa1889803c6c27ecb2517819f6bdc4a4379bb2
Dain M. Blodorn Kim <dain@db13.us>
2022-09-28 17:09:17 -0700

    10 convert array of drop contracts to playlist array object (#31)
    * audioplayer rendering array of editions contracts
    
    * rendering array of editions style contracts
    
    * ts ignore on formatting loop

--- examples/nextjs/src/pages/index.tsx
+++ examples/nextjs/src/pages/index.tsx
@@ -1,28 +1,23 @@
-import { AudioPlayer } from "@public-assembly/audio-player-ui";
-import { fakePlaylist } from "../fakePlaylist";
-import { useState } from "react";
-import { RawDisplayer } from "../components";
+import { EditionsAudioPlayer } from "@public-assembly/audio-player-ui";
+import { useContractRead } from 'wagmi'
+import Contract from './../abi/CurationManager.json'
 
 function Page() {
-  const [nft, setNFT] = useState(fakePlaylist[0]);
-  
+  const { data } = useContractRead({
+    addressOrName: "0x6422Bf82Ab27F121a043d6DE88b55FA39e2ea292", 
+    contractInterface: Contract.abi,
+    functionName: 'viewAllListings',
+  })
+
   return (
     <section className="flex flex-col gap-4">
       <div>
         <h1 className="text-xl mb-4">Consuming Curation Playlist</h1>
         <hr className="border border-b-0 border-dashed"/>
       </div>
-      <RawDisplayer data={fakePlaylist} />
-      {fakePlaylist.map((nft) => (
-        <div
-          key={nft.id}
-          className=" p-3 border cursor-pointer mb-4"
-          onClick={() => setNFT(nft)}
-        >
-          {nft.artist} - {nft.title}
-        </div>
-      ))}
-      <AudioPlayer playlist={fakePlaylist} nft={nft} />
+      {data && data.length && <EditionsAudioPlayer
+        contractAddresses={data as string[]}
+      />}
     </section>
   );
 }
