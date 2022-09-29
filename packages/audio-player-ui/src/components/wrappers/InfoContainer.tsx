964bb509080a3b53a7ed0686647cdc463e5ca362
Dain Blodorn Kim <dain@db13.us>
2022-09-28 17:21:56 -0700

    integrate wagmi

--- packages/audio-player-ui/src/components/wrappers/InfoContainer.tsx
+++ packages/audio-player-ui/src/components/wrappers/InfoContainer.tsx
@@ -1,15 +1,20 @@
 // @ts-ignore:next-line
 import * as React from 'react'
-
+import { useEnsName } from 'wagmi'
 import { usePlayerContext } from '../../context/AudioPlayerContext'
 
-export const AudioPlayerDisplayInfo = ({}) => {
+export const AudioPlayerDisplayInfo = () => {
   const { currentTrack } = usePlayerContext()
+
+  const { data: ensName } = useEnsName({
+    address: currentTrack?.artist,
+  })
+
   return (
     <div className="pa-audio-player__info-container col-span-2 ">
       <div className="pa-audio-player__info-container-items flex items-center">
         <span className="pa-audio-player__info-container-artist">
-          {currentTrack?.artist}
+          {ensName ? ensName : currentTrack?.artist}
         </span>
         <span className="pa-audio-player__info-container-hyphen mx-1">-</span>
         <span className="pa-audio-player__info-container-title truncate">
